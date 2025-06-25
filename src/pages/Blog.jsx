c13b0d2548d435641ca431a7f953f98461c8e48e
Rudransh Pandey <rdrnshpndy@gmail.com>
2025-06-25 17:04:59 +0530

    Update Blog.jsx

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import BlogPage from '../components/unoperated1'
 import { FaArrowRight } from 'react-icons/fa'
 import { Link } from 'react-router'
 import AllBlogs from './AllBlogs'
@@ -8,11 +7,11 @@ import AllBlogs from './AllBlogs'
 const Blog = () => {
   return (
     <>
-      <div className='relative px-4 py-33 mx-auto overflow-hidden'>
-            {/* Blurred starry background */}
-            <div className="absolute inset-0 w-full h-full bg-[url('/images/starry-bg.png')] bg-cover bg-center filter blur-md" />
+    <div className='#ff800'>
+      <div className='relative px-4 py-15 mx-auto overflow-hidden bg-black flex items-center justify-center'>
+
             <div className='relative z-10 text-white text-center'>
-              <h1 className='text-5xl lg:text-7xl leading-snug font-bold mb-5 font-michroma'>Welcome to our Blog</h1>
+              <h1 className='text-3xl lg:text-6xl leading-snug font-bold mb-5 font-michroma'>Welcome to our <span className='text-orange-500'>Blog</span></h1>
               <p className='text-gray-100 lg:w-3/5 mx-auto mb-5 font-michroma'>Stay updated with the latest news and insights related to colleges, exams and more</p>
               <a href="#ablogs">
                 <button className='bg-orange-500 px-6 py-2 font-medium mt-8 rounded hover:bg-white hover:text-orange-500 transition-all duration-200 ease-in inline-flex items-center justify-center gap-2'>
@@ -21,10 +20,9 @@ const Blog = () => {
               </a>
             </div>
       </div>
-      
-      <BlogPage />
 
       <AllBlogs />
+    </div>
     </>
   )
 }
